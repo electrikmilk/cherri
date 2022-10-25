895563193eb0afa35c53603b3b90b5f9c26f3935
electrikmilk <brandonjordan124@gmail.com>
2022-10-24 22:02:19 -0400

    Update usage example

--- args.go
+++ args.go
@@ -45,7 +45,7 @@ func init() {
 
 // Prints a usage message based on the arguments and usage you have registered.
 func usage() {
-	fmt.Print("USAGE: cherri [FILE] [options]")
+	fmt.Print("USAGE: cherri [FILE] [options: -s=, -u, -d, -o=]")
 	fmt.Printf("\nOptions:\n")
 	for _, arg := range registered {
 		fmt.Printf("\t-%s --%s\t%s\n", arg.short, arg.name, arg.description)
