0dc365c9d9a8ddc5723ca59fbed57ce3d227e81c
Brandon Jordan <brandonjordan124@gmail.com>
2024-04-30 17:23:15 -0400

    Implement HubSign as fallback for macOS signing
    The folks at RoutineHub have been gracious enough to allow Cherri to use
    their HubSign service to sign Shortcuts in the compiler.
    
    We were doing this previously as a necessity in the playground, but now
    the compiler will also fallback to using this service if macOS fails to
    sign the Shortcut.
    
    Obviously, this comes with a performance hit to compilation but is worth
    it in the end.

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/electrikmilk/args-parser"
 	"github.com/google/uuid"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
@@ -221,12 +224,50 @@ func sign() {
 	var signErr = sign.Run()
 	if signErr != nil {
 		if args.Using("debug") {
-			fmt.Print(ansi("failed!", red) + "\n")
+			fmt.Print(ansi("Failed!\n", red))
 		}
-		exit("Failed to sign Shortcut\n\nshortcuts: " + stdErr.String())
+
+		fmt.Printf("%s\n%s\n", ansi("Failed to sign Shortcut using macOS :(", red, bold), ansi(stdErr.String(), red))
+		hubsign()
 	}
+}
+
+const HubSignURL = "https://hubsign.routinehub.services/sign"
+
+// Sign the Shortcut using RoutineHub's signing service.
+func hubsign() {
 	if args.Using("debug") {
-		fmt.Println(ansi("Done.", green) + "\n")
+		fmt.Print("Attempting to sign using HubSign...")
+	}
+
+	var payload = map[string]string{
+		"shortcutName": basename,
+		"shortcut":     compiled,
+	}
+	var jsonPayload, jsonErr = json.Marshal(payload)
+	handle(jsonErr)
+
+	var request, httpErr = http.NewRequest("POST", HubSignURL, bytes.NewReader(jsonPayload))
+	handle(httpErr)
+	request.Header.Set("Content-Type", "application/json")
+
+	var client = &http.Client{}
+	response, resErr := client.Do(request)
+	handle(resErr)
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		exit(fmt.Sprintf("Failed to sign Shortcut (%s)", response.Status))
+	}
+
+	var body, readErr = io.ReadAll(response.Body)
+	handle(readErr)
+
+	var writeErr = os.WriteFile(outputPath, body, 0600)
+	handle(writeErr)
+
+	if args.Using("debug") {
+		fmt.Println(ansi("Done.", green))
 	}
 }
 
