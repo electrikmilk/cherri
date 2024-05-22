0a37f9a24e91095007442aa57951fac2f9df6109
Brandon Jordan <brandonjordan124@gmail.com>
2024-05-22 17:29:52 -0400

    bugfix #53: Fix variable references

--- plist.go
+++ plist.go
@@ -381,7 +381,7 @@ func inputValue(key string, name string, varUUID string) plistData {
 	var value []plistData
 	if varUUID != "" {
 		var variable = variables[name]
-		if !variable.repeatItem && variable.constant {
+		if !variable.repeatItem && ((variable.constant && variable.valueType == Variable) || variable.valueType != Variable) {
 			value = []plistData{
 				{
 					key:      "OutputName",
