882b68c231954b4b89b77b5ffde2c79a4cf8d070
Brandon Jordan <brandonjordan124@gmail.com>
2025-04-02 12:51:37 -0400

    Remove plist header and footer constants

--- plistgen.go
+++ plistgen.go
@@ -15,9 +15,6 @@ import (
 	plists "howett.net/plist"
 )
 
-const header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>\n"
-const footer = "</dict>\n</plist>\n"
-
 var plist strings.Builder
 var compiled string
 
