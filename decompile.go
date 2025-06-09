05e750d52d513c5790caba6b6f2e25e7bb72067e
Brandon Jordan <brandonjordan124@gmail.com>
2025-06-08 21:16:52 -0400

    Fix printing of debug action matches

--- decompile.go
+++ decompile.go
@@ -1313,9 +1313,9 @@ func matchSplitAction(splitActions *[]actionValue, parameters map[string]any, id
 	})
 
 	if args.Using("debug") {
-		for _, match := range matches[1:] {
+		for _, match := range matches[0:2] {
 			fmt.Printf("%s()\n", match.action.identifier)
-			fmt.Println("params:", match.params, ", values:", match.values)
+			fmt.Println("params:", match.params, " values:", match.values)
 			fmt.Println("---")
 		}
 		fmt.Print("\n\n")
