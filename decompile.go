e6035e4dcc269330ccf2c1d79e4a6bfc34edf407
Brandon Jordan <brandonjordan124@gmail.com>
2025-10-18 19:28:51 -0400

    Allow constant literal to have been used in a variable

--- decompile.go
+++ decompile.go
@@ -440,9 +440,6 @@ func checkConstantLiteral(action *ShortcutAction) {
 		return
 	}
 	var actionUUID = action.WFWorkflowActionParameters[UUID].(string)
-	if slices.Contains(varUUIDs, actionUUID) {
-		return
-	}
 	if outputName, found := uuids[actionUUID]; found {
 		newCodeLine(fmt.Sprintf("const %s = ", outputName))
 		code.WriteString(currentVariableValue)
